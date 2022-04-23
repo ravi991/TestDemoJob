460ee624a3328da8ecd2c311032c47e90886f3a6
ravi991 <raviranjancs4@gmail.com>
2022-04-23 10:44:56 -0400

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,19 @@
+
+public class Add {
+	public static void main(String[] args) {
+		int x =10;
+        int y =5;
+        int z;          
+        z = x+y; // + addition
+        System.out.println(z);
+        z = x-y; // - subtraction
+        System.out.println(z);
+        z =x*y; // * Multiplication
+        System.out.println(z);
+        z= x/y; // / Division
+        System.out.println(z);
+        z= x%y; // % Modulus
+        System.out.println(z);
+	}
+
+}
